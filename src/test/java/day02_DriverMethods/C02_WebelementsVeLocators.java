0c51a41c513255cb5a338277a22e6baa63621189
ayşe sena akyol <senaakyooll5@gmail.com>
2022-12-19 23:56:42 +0300

    19.12.2022

---
+++ src/test/java/day02_DriverMethods/C02_WebelementsVeLocators.java
@@ -0,0 +1,42 @@
+package day02_DriverMethods;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
+
+public class C02_WebelementsVeLocators {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "src/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        //Amazon sayfasına gidelim
+        driver.get("https://amazon.com");
+        //Search bölümünü locate edelim
+        //WebElement aramakutusu = driver.findElement(By.id("twotabsearchtextbox"));
+        WebElement aramakutusu = driver.findElement(By.name("field-keywords"));
+        //Search bolumunde iphone aratalım
+        aramakutusu.sendKeys("iphone", Keys.ENTER);
+        /*
+            Eğer bir webelementi (Web sayfasındaki her bir buton ya da text/yazı) locate (konumunu belirleme)
+            etmek istersek önce manuel olarak web sayfasını açıp sayfa üzerinde sağ click yapıp incele'ye tıklarız.
+            Karşımıza çıkan HTML kodlarından locater'lardan (konum belirleyiciler) eşsiz olanı seçeriz.
+            Genellikle id Attribute'u kullanılır. Seçtiğimiz attribute değerini findelement() methodu içine
+            findelement(By,id("attribute değeri")) şeklinde yazarız.
+         */
+
+        //Amazon sayfasındaki <input> ve <a> tag'larının sayısını yazdırınız
+        List<WebElement> inputTags = driver.findElements(By.tagName("input"));
+        System.out.println("Input Tag Sayısı = "+inputTags.size());
+
+        List<WebElement> linklerList = driver.findElements(By.tagName("a"));
+        System.out.println("Link Sayısı = "+linklerList.size());
+    }
+}
